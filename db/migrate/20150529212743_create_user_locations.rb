3ae9c5596d6b9aae25cbb3fb9885a089c780486a
Bala Subrahmanyam Varanasi <bala@tinyowl.co.in>
2015-05-30 02:57:59 +0530

    adds new model user location

---
+++ db/migrate/20150529212743_create_user_locations.rb
@@ -0,0 +1,11 @@
+class CreateUserLocations < ActiveRecord::Migration
+  def change
+    create_table :user_locations do |t|
+      t.integer :user_id
+      t.decimal :latitude, precision: 9, scale: 6
+      t.decimal :longitude, precision: 9, scale: 6
+
+      t.timestamps null: false
+    end
+  end
+end
---
+++ spec/factories/user_locations.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user_location do
+    user_id 1
+latitude "9.99"
+longitude "9.99"
+  end
+
+end
