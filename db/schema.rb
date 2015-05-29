2af2f0312984701dcbca391241aa837eb99dc027
Bala Subrahmanyam Varanasi <bala@tinyowl.co.in>
2015-05-30 03:05:29 +0530

    adds new user location table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150529174654) do
+ActiveRecord::Schema.define(version: 20150529213359) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+  enable_extension "postgis"
+
+  create_table "user_locations", force: :cascade do |t|
+    t.integer  "user_id"
+    t.decimal  "latitude",   precision: 9, scale: 6
+    t.decimal  "longitude",  precision: 9, scale: 6
+    t.datetime "created_at",                         null: false
+    t.datetime "updated_at",                         null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                          null: false
