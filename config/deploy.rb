cda42b7aff416de82dc9f9bb8e63f84c2419cedc
Bala Subrahmanyam Varanasi <bala@tinyowl.co.in>
2015-05-30 10:31:26 +0530

    change restart script

--- config/deploy.rb
+++ config/deploy.rb
@@ -27,7 +27,7 @@ namespace :deploy do
   desc "Restart application"
   task :restart do
     on roles(:app), in: :sequence, wait: 5 do
-      execute :touch, release_path.join("tmp/restart.txt")
+      sudo "service nginx restart"
     end
   end
 
