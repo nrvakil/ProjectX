e558073323bd7ef2d1ce036295c9f822851a58e5
Bala Subrahmanyam Varanasi <bala@tinyowl.co.in>
2015-05-30 12:38:15 +0530

    fixes routes errors

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
   root 'home#index'
+  resources :users
 
   get '/login' => 'users#authenticate_user'
   post '/set_location' => 'users_location#set_my_location'
