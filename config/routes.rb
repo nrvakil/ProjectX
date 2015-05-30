7a259ea612d234fb4658e3753c9c18d3f8f50a48
Bala Subrahmanyam Varanasi <bala@tinyowl.co.in>
2015-05-30 12:13:48 +0530

    fixes login url

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   root 'home#index'
 
-  get '/' => 'users#authenticate_user'
+  get '/login' => 'users#authenticate_user'
   post '/set_location' => 'users_location#set_my_location'
   get  '/get_nearby_people' => 'users_location#get_nearby_people'
   post '/hookup' => 'hook_up#hookup'
